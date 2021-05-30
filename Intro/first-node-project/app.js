5841779345689daf358051ad19b604dd826c3243
savan patel <savan9080@gmail.com>
2021-05-31 01:35:26 +0530

    31/5/2021

--- Intro/first-node-project/app.js
+++ Intro/first-node-project/app.js
@@ -1,21 +1,27 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bodyParser = require("body-parser");
+const expresHbs = require("express-handlebars");
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, 'public')));
+const adminData = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+const router = require("./routes/shop");
 
-app.use('/admin', adminRoutes);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  //   res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
+  res.status(404).render("404", { pageTitle: "Page Not Found", path: "/404" });
 });
 
 app.listen(3000);
