ed06a9271631f35f149db43d631545f0c49c9da7
savan patel <savan9080@gmail.com>
2021-05-31 02:02:46 +0530

    31/5/2021 - Assignment 3

--- Intro/Assignment_2/app.js
+++ Intro/Assignment_2/app.js
@@ -1,13 +1,16 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
-const mainRoutes = require('./routes/index');
+const mainRoutes = require("./routes/index");
 
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+// const app = express();
 
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.set("view engine", "ejs");
+app.set("views", "views");
 app.use(mainRoutes);
 
 app.listen(3000);
--- Intro/Assignment_2/routes/index.js
+++ Intro/Assignment_2/routes/index.js
@@ -1,15 +1,24 @@
-const path = require('path');
+const path = require("path");
 
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+// const
+router.get("/", (req, res, next) => {
+  //   res.sendFile(path.join(__dirname, "..", "views", "index.html"));
+  res.render("index", { pageTitle: "Main Page", mainHeading: "Main Content" });
 });
 
-router.get('/users', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'views', 'users.html'));
+router.post("/users", (req, res, next) => {
+  const usr_name = req.body.txt;
+  console.log(usr_name);
+  //   res.sendFile(path.join(__dirname, "..", "views", "users"));
+  res.render("users", {
+    pageTitle: "Users Page",
+    mainHeading: "Submitted User Name:",
+    userName: usr_name,
+  });
 });
 
 module.exports = router;
